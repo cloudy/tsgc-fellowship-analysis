d572eebcb365639a2b0bcbe6d99b9d42a3956034
cloudy <im@joe.cloud>
2019-06-13 21:58:22 -0400

    Add init data and analysis scripts

---
+++ scripts/scrape.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# scrape rules
+start_year = 2008
+end_year = 2018
+url_pre = 'http://www.tsgc.utexas.edu/grants/'
+award_type = 'scholars'
+all_data = pd.DataFrame()
+f_dir = ''
+
+for year in range(start_year, end_year + 1):
+    try:
+        print(year)
+        url = url_pre + str(year) + '/' + award_type + '.html'
+        df = pd.read_html(url, header=0)
+        df = df[np.argmax([len(d) for d in df])]
+        df['YEAR'] = year
+        all_data = all_data.append(df, sort=False, ignore_index=True)
+    except:
+        print('Year not found:',year)
+
+all_data.to_csv(f_dir+'tsgc-'+award_type+'-'+str(start_year)+'-'+str(end_year)+'.csv', index_label=False, index=False)
